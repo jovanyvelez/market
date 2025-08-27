f4279847b743f014635e06f6c19ec4c25abe33d6
jovany <jovany.velez@gmail.com>
2025-08-27 00:02:28 -0500

    vercel.json3

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ Sistema de gestión para market pequeño
 """
 
 from typing import Annotated
-from fastapi.staticfiles import StaticFiles
+
 from fastapi import FastAPI, Depends
-from src.api.v1.endpoints import categorias
-from src.api.v2.endpoints import categorias as categorias_v2
+from app.api.v1.endpoints import categorias
+from app.api.v2.endpoints import categorias as categorias_v2
 
 
 # Importar configuración y base de datos
 
-from src.core.database import get_session
+from app.core.database import get_session
 from sqlmodel import Session, select,text
 
-from src.models.models import Categoria
+from app.models.models import Categoria
 # Importar routers de la API
 
 
 # Crear la aplicación FastAPI
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
 app.include_router(categorias.router, prefix="/api/v1/categorias", tags=["categorias"])
 app.include_router(categorias_v2.router, prefix="/api/v2/categorias", tags=["categorias_v2"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI Market System"}
